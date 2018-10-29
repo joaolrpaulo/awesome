9a3641c016f36b133927ea2f4d8fded7ddf0847d
João Paulo <joaolrpaulo@gmail.com>
2018-10-29 07:58:50 +0000

    Change wallpaper and autorun
    Signed-off-by: João Paulo <joaolrpaulo@gmail.com>

--- autorun.sh
+++ autorun.sh
@@ -17,4 +17,4 @@ setxkbmap -layout us -variant mac
 run nm-applet
 run compton
 check-enpass
-xrandr --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-7 --off --output DP-6 --off --output DP-5 --mode 2560x1440 --pos 5120x0 --rotate normal --output DP-4 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-3 --off --output DP-2 --off --output DP-1 --mode 2560x1440 --pos 2560x0 --rotate normal
+xrandr --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-7 --off --output DP-6 --off --output DP-5 --mode 2560x1440 --pos 5120x0 --rotate normal --output DP-4 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-3 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-2 --off --output DP-1 --off
